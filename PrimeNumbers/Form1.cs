fc2f5db1f4328158d9d8ae21fb9e8322166dd27a
RicardoKrm <78158035+RicardoKrm@users.noreply.github.com>
2021-11-26 02:54:14 -0300

    Update Form1.cs

--- PrimeNumbers/Form1.cs
+++ PrimeNumbers/Form1.cs
@@ -8,7 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+//pan con queso
 namespace PrimeNumbers
 {
     public partial class Form1 : Form
